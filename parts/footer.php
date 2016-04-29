f2bec6a2c3aa6d0501b4ba0d0acf23c0b1bc0820
Craig Fowler <craig@wearegiants.com>
2016-04-28 18:42:13 -0700

    added footer

--- parts/footer.php
+++ parts/footer.php
@@ -1,8 +1,10 @@
 </div>
 </div>
 
-<div id="cart-sidebar" class="cart-toggle">
-
+<div id="cart-sidebar" class="cart-toggle fs-grid">
+  <div class="wrapper">
+    <a href="#" class="btn btn--full bg--black color--white">Checkout</a>
+  </div>
 </div>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
