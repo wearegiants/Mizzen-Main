1c643c381e2ce44c4f67dfe07509b8e99b508e2d
Craig Fowler <craig@wearegiants.com>
2016-05-26 16:41:44 -0700

    homepage updates

--- src/js/site/cartsize.js
+++ src/js/site/cartsize.js
@@ -3,7 +3,11 @@ function cartSize(){
   	var wh = $(window).height();
   	var ch = $('#cart-sidebar__header').height();
   	var cf = $('#cart-sidebar__footer').height();
-  	var ih = wh - (ch);
+  	var ih = wh - ch;
   	$('#cart-sidebar__content').css('height', ih);
+    console.log(wh);
+    console.log(ih);
   }).resize();
 }
+
+cartSize();
