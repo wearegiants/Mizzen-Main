0fd764fe1b7163c7cfdccd6501802c962cdd270a
Craig Fowler <craig@wearegiants.com>
2016-06-04 09:14:13 -0700

    added links to slides

--- src/js/site/default-ui.js
+++ src/js/site/default-ui.js
@@ -1,6 +1,7 @@
 $("input[type=checkbox], input[type=radio]").checkbox();
 $('.wallpaper').background();
 $('.carousel').carousel();
+$('.perf__carousel').carousel();
 $("input[type=number]").number();
 $(".swap").swap();
 $("#wrapper__content").on('click',function(){
