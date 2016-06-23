e6e2f2e34c9ebea6ffd79a2b05a2d1e2061af176
Craig Fowler <craig@wearegiants.com>
2016-06-23 08:01:20 -0700

    added about pages

---
+++ about.php
@@ -0,0 +1,141 @@
+<?php include('parts/header.php'); ?>
+
+<div id="index" class="fs-grid">
+
+<!-- Hero -->
+
+<div class="carousel">
+  <div class="carousel__slide">
+    <div class="home__hero hero hero--md bg--bgGray relative wallpaper" data-background-options='{"source":"/assets/index-slide-1-desktop.jpg"}'>
+      <div class="centered centered--full">
+        <div class="fs-row">
+          <div class="fs-cell fs-all-full">
+            <div class="text-right">
+              <h1>Diamond in the Scruff</h1>
+              <a href="#" class="btn btn--primary bg--white">Shop the Norfolk</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="carousel__slide">
+    <div class="home__hero hero hero--md bg--bgGray relative wallpaper" data-background-options='{"source":"/assets/Chinos_2048x2048.jpg"}'>
+      <div class="centered centered--full">
+        <div class="fs-row">
+          <div class="fs-cell fs-all-full">
+            <div class="text-right">
+              <h1>Diamond in the Scruff</h1>
+              <a href="#" class="btn btn--primary bg--white">Shop the Norfolk</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<hr class="invisible">
+
+<!-- Performance Tag -->
+
+<div class="home__performance">
+  <div class="fs-row"> 
+    <div class="fs-cell fs-lg-10 fs-lg-push-1 fs-md-4 fs-md-push-1 fs-sm-3">
+      <div class="text-center">
+        <h2>Peformance Fabric Menswear</h2>
+        <p>Our performance fabric apparel has the traditional look of menswear, but feels like the most comfortable athletic gear.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<hr class="invisible">
+
+<!-- Shop Section -->
+
+<div class="home__shop">
+  <div class="fs-row">
+    <div class="fs-cell fs-all-full">
+      <div class="hero hero--sm bg--black relative">
+        <div class='centered text-center'>
+          <a href="#" class="btn bg--black color--white">Shop Dress Shirts</a>
+        </div>
+      </div>
+    </div>
+    <hr class="invisible compact fs-cell fs-all-full">
+    <div class="fs-cell fs-lg-third fs-md-full fs-sm-full">
+      <div class="hero hero--xs bg--gray relative">
+        <div class='centered text-center'>
+          <a href="#" class="btn bg--black color--white">Shop Dress Shirts</a>
+        </div>
+      </div>
+    </div>
+    <div class="fs-cell fs-lg-third fs-md-full fs-sm-full">
+      <div class="hero hero--xs bg--bgGray relative">
+        <div class='centered text-center'>
+          <a href="#" class="btn bg--black color--white">Shop Dress Shirts</a>
+        </div>
+      </div>
+    </div>
+    <div class="fs-cell fs-lg-third fs-md-full fs-sm-full">
+      <div class="hero hero--xs bg--gray relative">
+        <div class='centered text-center'>
+          <a href="#" class="btn bg--black color--white">Shop Dress Shirts</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<hr class="invisible">
+
+<!-- Press -->
+
+<div class="home__press">
+  <div class="fs-row">
+    <div class="fs-cell fs-all-full">
+      <div class="carousel">
+        <div class="carousel__slide text-center">
+          <h1><em>Some press quote</em></h1>
+          <span>NYTimes</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<hr class="invisible">
+
+<!-- Magazine -->
+
+<div class="home__magazine">
+  <div class="fs-row">
+    <div class="fs-cell fs-lg-half fs-md-half fs-sm-full">
+      <div class="hero hero--md bg--bgGray relative">
+        <div class="centered">
+          <div class="wrapper--extra">
+            <h1>What is Mizzen+Main?</h1>
+            <p>Learn more about us and here's a second line to explain that</p>
+            <a href="#" class="btn bg--black color--white">Learn More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="fs-cell fs-lg-half fs-md-half fs-sm-full">
+      <div class="fs-row">
+        <div class="fs-cell fs-all-full">
+          <div class="hero hero--xs bg--gray"></div>
+        </div>
+        <hr class="invisible compact fs-cell fs-all-full">
+        <div class="fs-cell fs-all-full">
+          <div class="hero hero--xs bg--black"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<?php include('parts/footer.php'); ?>
